fbeddb9359413b8a28f84e930fd3bb41de779ace
ak10s <ak10s@mail.ru>
2022-09-15 21:27:59 +0500

    Добавлен def random_world

--- Fild_of_Dreams/check.py
+++ Fild_of_Dreams/check.py
@@ -3,5 +3,11 @@ import random
 list = ['Мама', 'Папа', 'Дедушка', 'Бабушка']
 
 
+def randon_world():
+    return random.choice(list)
+
+
+input('введите слово')
+
 if __name__ == '__main__':
-    print(random.choice(list))
+    print(randon_world())
