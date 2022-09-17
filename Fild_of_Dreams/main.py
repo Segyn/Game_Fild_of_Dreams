326eaa545b7b8e884d8865ab50f90034cba7d5f6
ak10s <ak10s@mail.ru>
2022-09-17 11:23:26 +0500

    Добавлен def word_user

--- Fild_of_Dreams/main.py
+++ Fild_of_Dreams/main.py
@@ -7,9 +7,12 @@ def randon_word():
     randon_word = random.choice(mylist)
     print(randon_word)
     return len(randon_word) * '*'
+
+
 def word_user():
-    user_word=input('Введите букву: ')
+    user_word = input('Введите букву: ')
     return user_word
+
 
 if __name__ == '__main__':
     print(randon_word())
