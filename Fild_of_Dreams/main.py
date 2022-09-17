7a4d14072dec1e0e7c5b0b044ab8facaced0ac5c
ak10s <ak10s@mail.ru>
2022-09-17 12:14:33 +0500

    Изменено использование случайного слова

--- Fild_of_Dreams/main.py
+++ Fild_of_Dreams/main.py
@@ -4,24 +4,24 @@ mylist = ['Мама', 'Папа', 'Дедушка', 'Бабушка']
 
 
 def create_randon_word() -> str:
-    randon_word1 = random.choice(mylist)
-    return randon_word1
+    randon_word_create = random.choice(mylist)
+    return randon_word_create
 
 
 randon_word = create_randon_word()
+print(randon_word)
 
 
-def hide_world(randon_word):
-    star = len(randon_word) * '*'
+def hide_world(randon_word1):
+    star = len(randon_word1) * '*'
     return star
 
 
-# def word_user() -> str:
-#     user_word = input('Введите букву: ')
-#     return user_word
+# def user_litter() -> str:
+#     litter_user = input('Введите букву: ')
+#     return litter_user
 
 
 if __name__ == '__main__':
-    print(create_randon_word())
     print(hide_world(randon_word))
     # print(word_user())
