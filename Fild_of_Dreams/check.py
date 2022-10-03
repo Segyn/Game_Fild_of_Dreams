5e82a1690fb1dd70e622d7679ffe0f44cd030be3
ak10s <ak10s@mail.ru>
2022-10-03 22:35:48 +0500

    тестирование основной механники

--- Fild_of_Dreams/check.py
+++ Fild_of_Dreams/check.py
@@ -2,10 +2,10 @@ randon_word = 'мама'
 litter_user = []
 
 
-def test(litter_user):
+def test(litter_user:str):
     a = input('введите букву ')
     litter_user.append(a)
-    return litter_user
+    test_litter_in_word()
 
 
 def test_litter_in_word():
@@ -16,13 +16,16 @@ def test_litter_in_word():
             word.append(i)
         else:
             word.append('*')
-    return word
+    new_word=''.join(word)
+    print(new_word)
+    if new_word == randon_word:
+        return print('Победа')
+    else: test(litter_user)
+
+
+
 
 
 if __name__ == '__main__':
-    # print(test_litter_in_word())
-    print(litter_user)
-    test(litter_user)
-    print(litter_user)
-    test(litter_user)
-    print(litter_user)
+    test_litter_in_word()
+
