4171198f5588cb5df45b08a701907eef787906f2
ak10s <ak10s@mail.ru>
2022-10-10 23:07:05 +0500

    Попытка проверки языка введенной буквы через (from langdetect import detect), неудачно. Проверяет только слова.

--- Fild_of_Dreams/check.py
+++ Fild_of_Dreams/check.py
@@ -1,4 +1,5 @@
 import random
+from langdetect import detect
 
 mylist = ['мама', 'папа', 'дедушка', 'бабушка']
 
@@ -7,18 +8,22 @@ randon_word_create = random.choice(mylist)
 
 def hide_word(litter_user):
     a = input('введите букву ')
+    print(detect(a))
     litter_user.append(a)
-    test_litter_in_word(litter_user)
+    return litter_user
 
 
-def test_litter_in_word(litter_user):
-    word = []
-    for i in randon_word_create:
-        word.append(i) if i in litter_user else word.append('*')
+#     test_litter_in_word(litter_user)
 
-    word = ''.join(word)
-    return print(word + '\nПобеда!') if word == randon_word_create else (print(word), hide_word(litter_user))
+
+# def test_litter_in_word(litter_user):
+#     word = []
+#     for i in randon_word_create:
+#         word.append(i) if i in litter_user else word.append('*')
+#
+#     word = ''.join(word)
+#     return print(word + '\nПобеда!') if word == randon_word_create else (print(word), hide_word(litter_user))
 
 
 if __name__ == '__main__':
-    test_litter_in_word([])
+    print(hide_word([]))
